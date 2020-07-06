88eea3db2a9cf021005bc4294572debe62fd931c
Orlov Alexander <Gl-lab@live.ru>
2020-07-06 19:51:44 +0300

    Уточнил задания и исправил

--- lab 1/lab1.py
+++ lab 1/lab1.py
@@ -2,11 +2,15 @@
 
 string = ''
 sum = 0
-string = input('Введите числа:')
-mass = string.split(' ')
-for char in mass:
-    if char.isdigit():
-        convertedChar = int(char)
-        if convertedChar % 2 == 0:
-            sum += int(convertedChar)
-print('Сумма четных:', sum)
+a = True
+while True:
+    string = input()
+    if string == '':
+        break
+#    if string.isnumeric():
+    convertedChar = int(string)
+    if convertedChar % 2 == 0:
+        sum += convertedChar
+print(sum)
+
+
--- strings/practic2.py
+++ strings/practic2.py
@@ -1,10 +1,10 @@
 #С клавиатуры вводится строка, а затем - подстрока. В строке найти все слова, в которых содержится заданная подстрока, и вывести эти слова целиком.
 
-inputString = input('Ввеите строку: ')
-findString = input('Введите подстроку: ')
+inputString = input()
+findString = input()
 
 wordList = inputString.split(' ')
 
 for word in wordList:
-    if word.find(findString) >= 0:
+    if word.lower().find(findString.lower()) >= 0:
         print(word)
